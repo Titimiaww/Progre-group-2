b5dc9028e55e65334d90fed566d8fab771e84278
Miaww`` <cac63096@gmail.com>
2024-12-13 20:25:18 +0700

    Update Transform.py

--- Transform.py
+++ Transform.py
@@ -45,7 +45,7 @@ if choice == "Home":
     with col1:
         st.image("Aep Saepudin.png", caption="Aep Saepudin")  # Pastikan gambar ada di folder yang sama atau gunakan path yang benar
     with col2:
-        st.image("Gambar WhatsApp 2024-12-13 pukul 19.55.18_8ebc55f9.jpg", caption="Aliffa Fiqria Wanda")  # Pastikan gambar ada di folder yang sama atau gunakan path yang benar
+        st.image("Aliffa Fiqria Wanda.jpg", caption="Aliffa Fiqria Wanda")  # Pastikan gambar ada di folder yang sama atau gunakan path yang benar
     with col3:
         st.image("Gambar WhatsApp 2024-12-13 pukul 19.55.17_dd4123ee.jpg", caption="Siti Khomsiah")  # Pastikan gambar ada di folder yang sama atau gunakan path yang benar
 
