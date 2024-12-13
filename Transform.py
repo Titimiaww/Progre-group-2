c17a69016aa713689461513d7e2087b71675d98b
Miaww`` <cac63096@gmail.com>
2024-12-13 21:47:29 +0700

    Update Transform.py

--- Transform.py
+++ Transform.py
@@ -37,7 +37,7 @@ st.title("Selamat datang di Website Transformasi Gambar")
 if choice == "Home":
     st.write("Pilih menu di sebelah kiri untuk memulai! 𓆝 𓆟 𓆞 𓆝")
     
-    st.subheader("Group 2 ≽^•⩊•^≼")
+    st.subheader("🐾『GROUP 2』 🐾")
     st.write("Industrial Engineering")
     st.write("Nama Members:")
     
